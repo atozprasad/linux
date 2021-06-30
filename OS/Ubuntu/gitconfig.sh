a4c4c25337b36796623df565de558274f03d46f8
atozprasad <atozcareer@gmail.com>
2021-06-30 15:24:04 +0200

    Basic git config setup

---
+++ OS/Ubuntu/gitconfig.sh
@@ -0,0 +1,4 @@
+git config --global user.name "atozprasad"
+git config --global user.email "atozcareer@gmail.com"
+git config --global credential.helper store
+#git config --global user.password "your password"
