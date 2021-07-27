ae8730356118864544dddc28fd15f78afeabd46e
atozprasad <atozcareer@gmail.com>
2021-07-27 22:49:56 +0200

    adding network packages to the list

--- OS/Ubuntu/packages.sh
+++ OS/Ubuntu/packages.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y git
 sudo apt-get install -y build-essential
 sudo apt-get install -y autoconf automake gdb git libffi-dev zlib1g-dev libssl-dev
 sudo apt-get install -y net-tools
-sudo apt install golang-go
+sudo apt install -y golang-go
+sudo apt  install -y nmap
+sudo apt install -y iputils-arping
 
 
